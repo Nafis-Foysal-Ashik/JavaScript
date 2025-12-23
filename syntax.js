b483d2dcd1184bfddea940500ace3129bf02c7a7
Nafis <nafisfoisal25@gmail.com>
2025-12-23 07:41:31 +0600

    first commit

---
+++ syntax.js
@@ -0,0 +1,26 @@
+/*
+//Variable nameing system:
+JavaScript also treats a dollar sign as a letter.
+Below are the valid variable naming convention:
+let $ = "Hello World"; ->ok
+let $$$ = 2; ->ok
+let $myMoney = 5; -> ok
+
+JavaScript treats underscore as a letter.
+Below are the valid variable naming convention:
+let _lastName = "Johnson"; ->ok
+let _x = 2; ->ok
+let _100 = 5; ->ok
+
+*/
+
+/*
+//String Operation
+
+We can also add strings, but strings will be concatenated:
+let x = "John" + " " + "Doe";->output : John Doe
+
+If we put a number in quotes the rest of the numbers will be treated as string and concatenated.
+let x =  2 + 3 + "5"; ->output : 55
+*/
+
